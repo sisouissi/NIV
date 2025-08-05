e273a2fc1b8debfea2648368d2f159b42a8c1201
sisouissi <zouhaier.souissi@gmail.com>
2025-08-05 16:15:20 +0100

    Add files via upload

---
+++ components/icons/Send.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Send = ({ className }: { className?: string }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}>
+    <path d="M22 2 11 13"/>
+    <polygon points="22 2 15 22 11 13 2 9 22 2"/>
+  </svg>
+);
---
+++ components/icons/Unplug.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+export const Unplug = ({ className }: { className?: string }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}>
+    <path d="M12 22v-5"/>
+    <path d="M9 17H7A5 5 0 0 1 7 7h10a5 5 0 0 1 0 10h-2"/>
+    <path d="m15 17-3-3-3 3"/>
+    <path d="M10 2H8"/>
+    <path d="M16 2h-2"/>
+  </svg>
+);
